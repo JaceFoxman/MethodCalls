0b6ec0acfccd35cdfd157c2d818e39fa929465c1
Jason Permann <JaceFoxman@gmail.com>
2025-02-03 08:54:08 -0700

    add myLuckyNumber

--- MethodCalls/MethodCalls.vb
+++ MethodCalls/MethodCalls.vb
@@ -9,15 +9,21 @@ Option Strict On
 Module MethodCalls
 
     Sub Main()
+        Dim myLuckyNumber As Integer = 7
 
         Console.WriteLine("Hello from Sub Main!!")
+        Console.WriteLine($"My lucky number is {myLuckyNumber}")
+        'just writing the name of the other sub will call it
+        MyOtherSub()
 
     End Sub
 
 
     'must have a name before clicking enter to get end sub to appear
     Sub MyOtherSub()
+        Dim myLuckyNumber As Integer = 5
         Console.WriteLine("Hello from my OtherSubMain!!")
+        Console.WriteLine($"My lucky number is {myLuckyNumber}")
     End Sub
 
 
