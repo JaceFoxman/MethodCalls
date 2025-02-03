9d13c940d606c10e69aabd7f4138459e5600f7ec
Jason Permann <JaceFoxman@gmail.com>
2025-02-03 08:44:11 -0700

    add another sub

--- MethodCalls/MethodCalls.vb
+++ MethodCalls/MethodCalls.vb
@@ -2,11 +2,24 @@
 'Spring 2025
 'RCET2265
 'Method Calls
+'https://github.com/JaceFoxman/MethodCalls.git
 
+Option Explicit On
+Option Strict On
 Module MethodCalls
 
     Sub Main()
 
+        Console.WriteLine("Hello from Sub Main!!")
+
     End Sub
+
+
+    'must have a name before clicking enter to get end sub to appear
+    Sub MyOtherSub()
+        Console.WriteLine("Hello from my OtherSubMain!!")
+    End Sub
+
+
 
 End Module
